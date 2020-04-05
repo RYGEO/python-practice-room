1bd9ff4b82fe70de62d030451bc02a7498d4314f
RYGEO <63171064+RYGEO@users.noreply.github.com>
2020-04-05 18:07:02 -0400

    Add files via upload

---
+++ backspace-and-quotation-marks.py
@@ -0,0 +1,29 @@
+"""
+In which I try to utilize the backspace function so I can add punctuation next to a variable without there being a space!
+
+Placing a + directly after a variable skips the space and lets you put whatever you want directly after it
+e.g.
+Input:
+	var = Ryan
+	print(var + ".")
+Output:
+	Ryan.
+
+Placing a forward slash in front of a quotation mark allows you to place it inside a printed statement
+e.g.
+Input:
+	print("\"Nice to meet you.\"")
+Output:
+	"Nice to meet you."
+
+
+Holy f@<%
+"""
+
+hao = "Jack"
+
+shu = "Jake"
+
+print("Hi, my name is", hao + ".")
+
+print("\"Pleased to meet you,\"", shu, "said.")
